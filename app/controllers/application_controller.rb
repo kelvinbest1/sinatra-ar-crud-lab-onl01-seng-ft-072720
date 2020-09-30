528d3bceb9f324fbd9861701f62ad8723d1603bc
kelvinbest1 <kelvinlanier@yahoo.com>
2020-09-30 16:04:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require_relative '../models/article.rb'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,5 +10,25 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
+  end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article1 = Article.create(title: params[:title], content: params[:content])
+    return to "articles/#{articles.id}"
+  end
+
+  get '/article/:id' do 
+    @article = Article.find(id: "#{params[:id]}")
+    erb :show
   end
 end
